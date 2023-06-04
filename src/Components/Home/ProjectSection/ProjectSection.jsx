1e9e7162605a1290c55fb4e757172da7054afb65
Ryoiki15 <hiirizkii@gmail.com>
2023-06-04 16:10:19 +0700

    fixing images again2x

--- src/Components/Home/ProjectSection/ProjectSection.jsx
+++ src/Components/Home/ProjectSection/ProjectSection.jsx
@@ -42,28 +42,34 @@ const ProjectSection = () => {
                 onSwiper={(swiper) => console.log(swiper)}
               >
                 <SwiperSlide className="h-[27rem]   swiper-slide flex justify-center items-center">
-                  <div className=" py-10 flex flex-col justify-center">
+                  <div className=" py-10 flex flex-col justify-center items-center">
                     <div className="w-60 h-60  bg-blue-300 mx-auto overflow-hidden">
                       <img className="scale-150 mt-10" src="/images/project1.jpeg" alt="" />
                     </div>
-                    <h1 className="text-center my-5 text-1xl font-bold ">Narasi Festival</h1>
-                    <p className="text-center mx-10">Menjadi koordinator dan operational control di event Narasi Festival by Najwa Shihab di jakarta</p>
+                    <div className="flex flex-col justify-center  h-40">
+                      <h1 className="text-center my-5 text-1xl font-bold ">Narasi Festival</h1>
+                      <p className="text-center mx-10">Menjadi koordinator dan operational control di event Narasi Festival by Najwa Shihab di jakarta</p>
+                    </div>
                   </div>
                 </SwiperSlide>
                 <SwiperSlide className="h-[27rem]   swiper-slide flex justify-center items-center">
                   <div className=" py-10 flex flex-col justify-center">
-                    <div className="w-60 h-60  bg-green-300 mx-auto overflow-hidden">
+                    <div className="w-60 h-60   mx-auto overflow-hidden">
                       <img className="scale-110 mt-2" src="/images/project2.jpeg" alt="" />
                     </div>
-                    <h1 className="text-center my-5 text-1xl font-bold ">Jajarans Festival</h1>
-                    <p className="text-center mx-10">Merencanakan alur operasional selama event berlangsung</p>
+                    <divc className="flex flex-col   h-40">
+                      <h1 className="text-center my-5 text-1xl font-bold ">Jajarans Festival</h1>
+                      <p className="text-center mx-10">Merencanakan alur operasional selama event berlangsung</p>
+                    </divc>
                   </div>
                 </SwiperSlide>
                 <SwiperSlide className="h-[27rem]   swiper-slide flex justify-center items-center">
                   <div className=" py-10 flex flex-col justify-center">
                     <div className="w-60 h-60  bg-red-300 mx-auto"></div>
-                    <h1 className="text-center my-5 text-1xl font-bold ">Project one</h1>
-                    <p className="text-center mx-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <div className="flex flex-col h-40">
+                      <h1 className="text-center my-5 text-1xl font-bold ">Project one</h1>
+                      <p className="text-center mx-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
                   </div>
                 </SwiperSlide>
               </Swiper>
